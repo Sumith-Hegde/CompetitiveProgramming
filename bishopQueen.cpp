4e46169b0cc003dc07f54797f0855fff628d501e
Sumith <hegdesumit1908@gmail.com>
2021-07-13 17:04:03 +0530

    bishop-queen problem(solved)

--- bishopQueen.cpp
+++ bishopQueen.cpp
@@ -5,45 +5,35 @@
 //                                 (iii)both bishop&queen ->"%"
 //                                 (iv)none->"_"
 #include<iostream>
+#include<list>
 using namespace std;
 char a[100][100]={'\0'};
 int m,n;
+list<pair<int,int>> Q;
+list<pair<int,int>> B;
 //checks if i,j is attacked by any of the placed bishop
 int bishop(int i,int j)
 {
-    // goes through entire board and searches for bishops
-     for(int k=0;k<m;k++)
-     {
-         for(int l=0;l<n;l++)
+         for(auto k:B)
          {
-             if(a[k][l]=='B')
-             {
-                //  checks if i,j is in the diagonal of found bishop
-                 if(labs(k-i)==labs(l-j))
+                //  checks if i,j is in the diagonal of any bishop
+                 if(labs(k.first-i)==labs(k.second-j))
                  {
                      return 1;
                  }
-             }
          }
-     }
+
      return 0;
 }
 //checks if i,j is attacked by any of the placed queens
 int queen(int i,int j)
 {
-    // goes through entire board and searches for queens
-    for(int k=0;k<m;k++)
+    for(auto k:Q)
     {
-        for(int l=0;l<n;l++)
+        //  checks if i,j is in the diagonal,row,column of any queen
+        if(labs(k.first-i)==labs(k.second-j)||k.first==i||k.second==j)
         {
-            if(a[k][l]=='Q')
-            {
-                //  checks if i,j is in the diagonal of found bishop
-                if(labs(k-i)==labs(l-j)||k==i||l==j)
-                {
-                    return 1;
-                }
-            }
+            return 1;
         }
     }
     return 0;
@@ -51,29 +41,30 @@ int queen(int i,int j)
 int main()
 {
     int q,b,x,y;
-    // printf("enter dimensions(with space)\n");
-    // scanf("%d %d",&m,&n);
-    // printf("enter number of bishops and queens respectively(with space)\n");
-    // scanf("%d %d",&b,&q);
     cout<<"enter dimensions(seperated by a space)"<<endl;
     cin>>m;
     cin>>n;
     cout<<"enter number of bishops and queens(seperated by a space)"<<endl;
-    cin>>m;
-    cin>>n;
+    cin>>b;
+    cin>>q;
     for(int i=0;i<q;i++)
     {
-        printf("enter x and y coordinates of queen %d(respectively with a space)",i+1);
-        scanf("%d %d",&x,&y);
+        cout<<"enter x and y coordinates of queen "<<i+1<<endl;
+        cin>>x;
+        cin>>y;
         a[x][y]='Q';
+        Q.push_back(make_pair(x,y));
     }
     for(int i=0;i<b;i++)
     {
-        printf("enter x and y coordinates of bishop %d(respectively with a space)",i+1);
-        scanf("%d %d",&x,&y);
+        cout<<"enter x and y coordinates of bishop "<<i+1<<endl;
+        cin>>x;
+        cin>>y;
         a[x][y]='B';
+        B.push_back(make_pair(x,y));
     }
     //  already filled condition and invalid coordinates not considered
+    //filling board according to question
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
@@ -100,6 +91,7 @@ int main()
             
         }
     }
+    // printing the board
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
