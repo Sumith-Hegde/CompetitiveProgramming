62b07435b8ddfcb6c993d30abb82dbc85c1f011c
Sumith <hegdesumit1908@gmail.com>
2021-08-11 11:04:13 +0530

    shortest reach and max connected cell solved

---
+++ bfs-shortest-reach_solution.cpp
@@ -0,0 +1,112 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Graph {
+    public:
+        vector<bool> visited;
+        vector<int> shortestDistance;
+        map<int,vector<int>> adj;
+        queue<int> Q;
+        int n;
+        Graph(int n) {
+            // vector<bool> temp(n+1,false);
+            // vector<int> temp2(n+1,-1);
+            // shortestDistance=temp2;
+            // visited=temp;
+            for(int i=0;i<n;i++)
+            {
+                visited.push_back(false);
+            }
+            for(int i=0;i<n;i++)
+            {
+                shortestDistance.push_back(-1);
+            }
+            this->n=n;
+        }
+    
+        void add_edge(int u, int v) {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        void BFS()
+        {
+            int current=Q.front();
+            int level=shortestDistance[current]+1;
+            Q.pop();
+            for(auto i: adj[current])
+            {
+                if(!visited[i])
+                {
+                    shortestDistance[i]=level;
+                    Q.push(i);
+                    visited[i]=true;
+                }
+            }
+            if(!Q.empty())
+            {
+                BFS();
+            }
+        }
+        vector<int> shortest_reach(int start) {
+            visited[start]=true;
+            Q.push(start);
+            shortestDistance[start]=0;
+            BFS();
+            vector<int> shortest_reach;
+            for(int i=0;i<n;i++)
+            {
+                    if(shortestDistance[i]!=-1)
+                    {
+                        shortest_reach.push_back(shortestDistance[i]*6);
+                    }
+                    else
+                    {
+                        shortest_reach.push_back(shortestDistance[i]);
+                    }
+            }
+            return shortest_reach;
+        }
+    
+};
+
+int main() {
+    int queries;
+    cin >> queries;
+        
+    for (int t = 0; t < queries; t++) {
+      
+		int n, m;
+        cin >> n;
+        // Create a graph of size n where each edge weight is 6: 
+        Graph graph(n);
+        cin >> m;
+        // read and set edges
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            u--, v--;
+            // add each edge to the graph
+            graph.add_edge(u, v);
+        }
+		int startId;
+        cin >> startId;
+        startId--;
+        // Find shortest reach from node s
+        vector<int> distances = graph.shortest_reach(startId);
+
+        for (int i = 0; i < distances.size(); i++) {
+            if (i != startId) {
+                cout << distances[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
---
+++ connected-cell-in-a-grid_solution.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'maxRegion' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY grid as parameter.
+ */
+bool valid(int x,int y,vector<vector<int>> &grid)
+{
+    int n=grid.size();
+    int m=grid[0].size();
+    if(x<0||x>n-1||y<0||y>m-1||grid[x][y]==0)
+    {
+        return false;
+    }
+    return true;
+}
+int DFS(int x,int y,vector<vector<int>> &visited,vector<vector<int>> &grid)
+{
+    int count=1;
+    visited[x][y]=1;
+    vector<pair<int,int>> neighbours;
+    neighbours.push_back(make_pair(x-1, y));
+    neighbours.push_back(make_pair(x-1, y-1));
+    neighbours.push_back(make_pair(x-1, y+1));
+    neighbours.push_back(make_pair(x, y-1));
+    neighbours.push_back(make_pair(x, y+1));
+    neighbours.push_back(make_pair(x+1, y+1));
+    neighbours.push_back(make_pair(x+1, y));
+    neighbours.push_back(make_pair(x+1, y-1));
+    for(auto i : neighbours)
+    {
+        if(valid(i.first,i.second,grid)&&!visited[i.first][i.second])
+        {
+            count+=DFS(i.first,i.second,visited,grid);
+        }
+    }
+    return count;
+}
+int maxRegion(vector<vector<int>> grid) {
+    // queue<pair<int,int>> Q;
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> visited(n,vector<int>(m,0));
+    vector<int> nodesInComponent;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(!visited[i][j]&&valid(i,j,grid))
+            {
+                nodesInComponent.push_back(DFS(i,j,visited,grid));
+            }
+        }
+    }
+    int largest=INT32_MIN;
+    for(auto i:nodesInComponent)
+    {
+        if(i>largest)
+        {
+            largest=i;
+        }
+    }
+    return largest;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string m_temp;
+    getline(cin, m_temp);
+
+    int m = stoi(ltrim(rtrim(m_temp)));
+
+    vector<vector<int>> grid(n);
+
+    for (int i = 0; i < n; i++) {
+        grid[i].resize(m);
+
+        string grid_row_temp_temp;
+        getline(cin, grid_row_temp_temp);
+
+        vector<string> grid_row_temp = split(rtrim(grid_row_temp_temp));
+
+        for (int j = 0; j < m; j++) {
+            int grid_row_item = stoi(grid_row_temp[j]);
+
+            grid[i][j] = grid_row_item;
+        }
+    }
+
+    int res = maxRegion(grid);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+ 
